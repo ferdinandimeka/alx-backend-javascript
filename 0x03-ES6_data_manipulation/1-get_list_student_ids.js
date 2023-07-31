1578e78e0b89b044592a7ef1b741671db04e57de
unknown <ferdinandimeka20@gmail.com>
2023-07-31 20:27:51 +0100

    update

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -9,10 +9,8 @@
  * @returns
  */
 export default function getListStudentIds(students) {
-  
   if (students instanceof Array) {
     return students.map((student) => student.id);
   }
-
   return [];
 }
