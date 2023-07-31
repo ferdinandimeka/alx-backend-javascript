b16a966a1c896a2a350c01b9cd4fafbc42ed4595
unknown <ferdinandimeka20@gmail.com>
2023-07-31 17:16:22 +0100

    Map filter reduce

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,18 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Ferdinand Imeka<https://github.com/ferdinandimeka/>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+
+  return [];
+}
