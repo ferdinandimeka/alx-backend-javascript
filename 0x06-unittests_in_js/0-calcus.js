ce4aec5c832bd45a04444db8abe507706361ba2e
root <root@ferdinandimeka>
2023-09-21 18:34:28 +0100

    unittests_in_js

--- 0x06-unittests_in_js/0-calcus.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number} a
- * @param {number} b
- * @returns {number} a + b
- */
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
-
-module.exports = calculateNumber;
